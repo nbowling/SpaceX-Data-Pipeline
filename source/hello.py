3862a7aa73b2a25cc66f30d2b05bee6388b488ee
Nick Bowling <nick.bowling@hotmail.co.uk>
2024-11-05 12:19:17 +0000

    Variables commit

---
+++ source/hello.py
@@ -0,0 +1,16 @@
+print("Hello, SpaceX Data Engineer!")
+# Integer
+launch_count = 10
+# Float
+sucess_rate = 0.95
+# String
+rocket_name = "Falcon 9"
+# Boolean
+is_active = True
+
+# Basic actions
+total_launches = launch_count + 5
+print(total_launches)
+
+# Output: 15
+
